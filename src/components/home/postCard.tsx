30d76931abbe1f9dc240bd7580e57a77e38cf913
wonder-lee <hi.wonder.dev@gmail.com>
2024-01-31 20:34:15 +0900

    [240131] 영상 목록 레이아웃 변경

--- src/components/home/postCard.tsx
+++ src/components/home/postCard.tsx
@@ -6,29 +6,43 @@ interface Props {
   data: any;
 }
 
-const PostCard = ({ data: { title, channelName, thumbnails } }: Props) => {
+const PostCard = ({
+  data: {
+    title,
+    channelName,
+    thumbnails,
+    subscribers,
+    viewCount,
+    publishedTime,
+  },
+}: Props) => {
   return (
     <div className="min-h-[120px] pt-4 border-solid border-b">
-      <div className="flex flex-row gap-3">
+      <div className="flex flex-row gap-3 justify-between">
+        <div>
+          <div className="text-[15px] min-h-[50px] font-medium">
+            <h2 className="break-keep">{truncateString(title, 33)}</h2>
+          </div>
+          <p className="text-[12px] text-gray-500">
+            {truncateString(channelName, 10)}
+          </p>
+          <div className="text-[12px] text-gray-500 flex gap-1">
+            <span>{subscribers.toLocaleString()}명</span>/
+            <span>{viewCount.toLocaleString()}회</span>/
+            <span>{publishedTime}</span>
+          </div>
+        </div>
         {thumbnails.length > 0 && (
           <div className="flex flex-col gap-1 items-center">
             <Image
-              className="w-[80px] h-[44px] object-cover object-center rounded-md"
+              className="w-[100px] h-[50px] object-cover object-center rounded-md"
               src={thumbnails[0].url}
-              width={160}
-              height={88}
+              width={180}
+              height={108}
               alt={title}
             />
-            <p className="text-[12px] text-gray-500">
-              {truncateString(channelName, 10)}
-            </p>
           </div>
         )}
-        <div>
-          <div className="text-[15px] min-h-[50px] font-medium">
-            <h2 className="break-keep">{truncateString(title, 30)}</h2>
-          </div>
-        </div>
       </div>
     </div>
   );
