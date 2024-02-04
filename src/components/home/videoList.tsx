e85f8732e4fb4016e373342b7fd370b0197b374a
wonder-lee <hi.wonder.dev@gmail.com>
2024-02-04 13:52:39 +0900

    [240204] videoList key 추가

--- src/components/home/videoList.tsx
+++ src/components/home/videoList.tsx
@@ -9,7 +9,7 @@ import PostCard from "@/components/home/postCard";
 import PostSkeleton from "@/components/common/postSkeleton";
 
 const VideoList = () => {
-  const router = useRouter();
+  const { push } = useRouter();
   const videoList = useRecoilValue(videoListAtom);
   const isSearch = useRecoilValue(isSearchAtom);
   const setSeletedVideoIndex = useSetRecoilState(selectedVideoIndexAtom);
@@ -20,14 +20,17 @@ const VideoList = () => {
 
   useEffect(() => {
     if (!isSearch) {
-      router.push("/");
+      push("/");
     }
   }, [isSearch]);
   return (
     <div>
       {videoList.length > 0
         ? videoList.map((data: any, index: number) => (
-            <div onClick={() => onClickVideoCard(index)}>
+            <div
+              onClick={() => onClickVideoCard(index)}
+              key={index + data.videoId}
+            >
               <PostCard data={data} key={data.title} index={index} />
             </div>
           ))
